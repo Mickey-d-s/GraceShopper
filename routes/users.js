764d191af0c4cd6c8ac475b39319ecac114c7c78
briankim <brianhkim13@gmail.com>
2023-06-20 14:52:04 -0500

    working on register route

--- routes/users.js
+++ routes/users.js
@@ -14,11 +14,50 @@ usersRouter.get("/", async (req, res, next) => {
     next(error);
   }
 });
-usersRouter.post("/login", async (req, res, next) => {
+usersRouter.post("/register", async (req, res, next) => {
   try {
     const { username, password } = req.body;
 
+    const _user = await getUserByUsername(username);
+    if (_user) {
+      next({
+        message: "That user already exists!",
+        name: " Auth Error",
+      });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    console.log("hashed password:", hashedPassword);
+    const user = await createUser({ username, password: hashedPassword });
+
+    console.log("JWT secret:", JWT_SECRET);
+
+    const token = jwt.sign(user, JWT_SECRET);
+    console.log("token:", token);
+
+    res.cookie("token", token, {
+      sameSite: "strict",
+      httpOnly: true,
+      signed: true,
+    });
+    delete user.password;
+    res.send({
+      success: true,
+      message: "Thank you for regisering",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
     const user = await getUserByUsername(username);
+    console.log("password:", password);
+    console.log("userpassword:", user.password);
     const checkedpassword = await bcrypt.compare(password, user.password);
     if (checkedpassword) {
       delete user.password;
