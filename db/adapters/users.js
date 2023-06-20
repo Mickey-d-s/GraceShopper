6d7b598748768051bad9afee56b8e1a7a0844bdd
briankim <brianhkim13@gmail.com>
2023-06-20 16:19:57 -0500

    save point

--- db/adapters/users.js
+++ db/adapters/users.js
@@ -1,6 +1,5 @@
 const client = require("../client");
 
-
 async function createUser({ username, email, password }) {
   try {
     console.log({ username, email, password });
@@ -13,22 +12,28 @@ async function createUser({ username, email, password }) {
             ON CONFLICT (username) DO NOTHING
             returning *;
 `,
-    [username, email, password]
-  );
-  return user;
+      [username, email, password]
+    );
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 }
-
 
 async function getUserByUsername(username) {
-  const {
-    rows: [user],
-  } = await client.query(
-    `SELECT * FROM users WHERE username=$1;
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `SELECT * FROM users WHERE username=$1;
         `,
-    [username]
-  );
-  console.log("getUserByUsername", user);
-  return user;
+      [username]
+    );
+    console.log("getUserByUsername", user);
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-  module.exports = { createUser, getUserByUsername };
+module.exports = { createUser, getUserByUsername };
