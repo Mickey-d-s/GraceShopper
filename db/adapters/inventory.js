6931c44d0c4d126879879011cb07a24a68846ec9
Elliot016 <elliot.carmona016@gmail.com>
2023-06-23 13:21:12 -0500

    Finished updateInventory, deleteInventory, getInventorybyId, getAllInventoryBYID

--- db/adapters/inventory.js
+++ db/adapters/inventory.js
@@ -17,4 +17,57 @@ async function createInventories({ product_id, quantity }) {
     throw error;
   }
 }
-module.exports = { createInventories };
+
+async function getInventoryById(id) {
+  const {
+    rows: [inventory],
+  } = await client.query(
+    `SELECT * FROM inventories WHERE inventory_id=$1;
+  `[id]
+  );
+  console.log("getInventoriesById CHECK", inventory);
+  return inventory;
+}
+
+async function getAllInventoryById() {
+  const {
+    rows: [inventory],
+  } = await client.query(`
+  SELECT * 
+  FROM inventories
+  `);
+  return rows;
+  console.log("getAllInventoryById CHECK", inventory);
+}
+
+function updateInventory({ inventory_id, product_id, quantity }) {
+  const {
+    rows: [inventory],
+  } = client.query(
+    `
+  UPDATE inventories SET inventory_id = $1
+    where product_id=$2 and quantity = $3;
+    `,
+    [inventory_id, product_id, quantity]
+  );
+  console.log("updateInventory CHECK", inventory);
+  return inventory;
+}
+
+async function deleteInventory(id) {
+  const {
+    rows: [inventory],
+  } = await client.query(
+    `DELETE from inventories WHERE inventory_id=$1
+  `,
+    [id]
+  );
+  return inventory;
+}
+module.exports = {
+  createInventories,
+  getInventoryById,
+  getAllInventoryById,
+  updateInventory,
+  deleteInventory,
+};
