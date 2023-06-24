fdf95a5a4f2e03e20ffbac2473aac1870c34677e
Charlotte Sass <Jeffreymsass@gmail.com>
2023-06-24 09:35:44 -0400

    working on home page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "../src/components/App";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./components/auth/AuthProvider.jsx";
-
+import "./App.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
