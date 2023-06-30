b90943c9b640f55c83f63d2c3f645fd65cd83ad3
Brandon <bbrungardt02@gmail.com>
2023-06-29 21:28:51 -0500

    started add to cart button

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -8,3 +8,26 @@ export async function fetchAllProducts() {
     console.error(error);
   }
 }
+
+// export async function createShoppingCart() {}
+
+export async function addCart_Item({ shoppingcart_id, product_id, count }) {
+  try {
+    const response = await fetch("/api/cart_items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ shoppingcart_id, product_id, count }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to add item to cart");
+    }
+
+    const cart_item = await response.json();
+    return cart_item;
+  } catch (error) {
+    throw error;
+  }
+}
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
-import { fetchAllProducts } from "../api/products";
+import { fetchAllProducts, addCart_Item } from "../api/products";
 
 export default function allProducts() {
   const [products, setProducts] = useState([]);
@@ -16,6 +16,19 @@ export default function allProducts() {
     }
     fetchProducts();
   }, []);
+  //  Need shopping cart to exist to addToCart
+  const addToCart = async (shoppingcart_id, product_id, count) => {
+    try {
+      const cartItem = await addCart_Item({
+        shoppingcart_id,
+        product_id,
+        count,
+      });
+      console.log("Item added to cart:", cartItem);
+    } catch (error) {
+      console.log("Failed to add item to cart:", error);
+    }
+  };
 
   // Group products by category
   const groupedProducts = products.reduce((acc, product) => {
@@ -37,6 +50,13 @@ export default function allProducts() {
               <h3>{product.product_name}</h3>
               <p>Price: ${product.price}</p>
               <p>Description: {product.description}</p>
+              <button
+                onClick={() =>
+                  addToCart(shoppingcart_id, product.product_id, 1)
+                }
+              >
+                Add to Cart
+              </button>{" "}
             </div>
           ))}
         </div>
