135552a04ee998e1fc0b6a962e23683d82c35dea
Brandon <bbrungardt02@gmail.com>
2023-06-30 17:49:06 -0500

    save point shopping cart api

--- client/src/api/shoppingcart.js
+++ client/src/api/shoppingcart.js
@@ -1,20 +1,40 @@
-export async function createShoppingCart(cartData) {
+// export async function createShoppingCart(cartData) {
+//   try {
+//     const response = await fetch("/shoppingcart", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(cartData),
+//     });
+
+//     if (!response.ok) {
+//       throw new Error("Failed to create shopping cart");
+//     }
+
+//     const createdCart = await response.json();
+//     return createdCart;
+//   } catch (error) {
+//     throw new Error(error.message);
+//   }
+// }
+
+export async function createShoppingCart({ status, user_id }) {
   try {
-    const response = await fetch("/shoppingcart", {
+    const response = await fetch(`/api/shoppingcart`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(cartData),
+      body: JSON.stringify({
+        status,
+        user_id,
+      }),
     });
-
-    if (!response.ok) {
-      throw new Error("Failed to create shopping cart");
-    }
-
-    const createdCart = await response.json();
-    return createdCart;
+    const result = await response.json();
+    console.log(result);
+    return result;
   } catch (error) {
-    throw new Error(error.message);
+    console.error(error);
   }
 }
--- client/src/components/ShoppingCart.jsx
+++ client/src/components/ShoppingCart.jsx
@@ -1,19 +1,26 @@
 import { useState, useEffect } from "react";
 // // import { useNavigate } from "react-router-dom";
 import { createShoppingCart } from "../api/shoppingcart";
+import AuthProvider from "./auth/AuthProvider";
+import useAuth from "./hooks/useAuth";
 
 export default function startOrder() {
   const [order, setOrder] = useState([]);
+  const { user, setUser } = useAuth;
+  const [status, setStatus] = useState("active");
 
   async function shoppingCart() {
     try {
-      const order = await createShoppingCart();
+      console.log("user", user);
+      const order = await createShoppingCart(status, user.id);
       setOrder(order);
+      setStatus(status);
     } catch (error) {
       console.log(error);
     }
   }
   shoppingCart();
+
   return (
     <div>
       <button onClick={() => shoppingCart()}>Create Shopping Cart</button>
