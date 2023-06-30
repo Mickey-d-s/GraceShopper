38ef05594eb4d919279f4314977e06bd2271a643
Brandon <bbrungardt02@gmail.com>
2023-06-29 20:48:44 -0500

    added mickey d logo and changed tab title to mickey d, also organized menu products by category

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -17,14 +17,28 @@ export default function allProducts() {
     fetchProducts();
   }, []);
 
+  // Group products by category
+  const groupedProducts = products.reduce((acc, product) => {
+    if (!acc[product.category]) {
+      acc[product.category] = [];
+    }
+    acc[product.category].push(product);
+    return acc;
+  }, {});
+
   return (
     <div>
-      <h2>Menu List</h2>
-      {products.map((product) => (
-        <div key={product.product_id}>
-          {product.product_name}
-          <p>Price: {product.price}</p>
-          <p>Description: {product.description}</p>
+      <h2>Menu</h2>
+      {Object.entries(groupedProducts).map(([category, products]) => (
+        <div key={category}>
+          <h1>{category}</h1>
+          {products.map((product) => (
+            <div key={product.product_id}>
+              <h3>{product.product_name}</h3>
+              <p>Price: ${product.price}</p>
+              <p>Description: {product.description}</p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
