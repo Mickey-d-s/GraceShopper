3c739657bf142b6e2655e6e126de25e91f8cacc0
briankim <brianhkim13@gmail.com>
2023-07-03 16:44:03 -0500

    fetchAllShoppingCarts api function complete

--- client/src/api/shoppingcart.js
+++ client/src/api/shoppingcart.js
@@ -17,3 +17,26 @@ export async function createShoppingCart({ status, user_id }) {
     console.error(error);
   }
 }
+
+export async function fetchAllShoppingCarts() {
+  try {
+    const response = await fetch("/api/shoppingcart");
+    const shoppingcarts = await response.json();
+
+    // Fetch product details for each inventory
+    const shoppingcartsPromises = shoppingcarts.map(async (shoppingcart) => {
+      const userResponse = await fetch(
+        `/api/shoppingcart/${shoppingcart.user_id}`
+      );
+      const user = await userResponse.json();
+      shoppingcart.user = user;
+      return shoppingcart.user;
+    });
+
+    const fetchedShoppingcart = await Promise.all(shoppingcartsPromises);
+    console.log("fetched Shoppingcart", fetchedShoppingcart);
+    return fetchedShoppingcart;
+  } catch (error) {
+    console.error(error);
+  }
+}
