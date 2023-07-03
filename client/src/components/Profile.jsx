9c5cf0928ad494240a8baac310fbed6ba4588f84
briankim <brianhkim13@gmail.com>
2023-07-03 17:04:26 -0500

    added to profile component, shopping cart table

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,15 +1,37 @@
 import useAuth from "../components/hooks/useAuth";
 import { useState, useEffect } from "react";
+import { fetchAllShoppingCarts } from "../api/shoppingcart";
 
 export default function Profile() {
-  const [user, setUser] = useState(useAuth());
-  console.log("user:", user);
+  const [ShoppingCarts, setShoppingCarts] = useState([]);
+
+  useEffect(() => {
+    const [user, setUser] = useState(useAuth());
+    console.log("user:", user);
+    async function fetchShoppingCarts() {
+      try {
+        const fetchedShoppingcarts = await fetchAllShoppingCarts();
+        setShoppingCarts(fetchedShoppingcarts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchInventories();
+  }, []);
 
   return (
     <>
       <div className="profilePage">
         <h1> Welcome to your Profile {user.username}</h1>
         <h2> order history</h2>
+        <h2>Inventory</h2>
+        {ShoppingCarts.map(() => (
+          <div key={shoppingcarts.shoppingcart_id}>
+            <p>Shopping Cart Id: {shoppingcarts.shoppingcart_id}</p>
+            <p>Status: {shoppingcarts.status}</p>
+            <p>User Id: {shoppingcarts.user_id}</p>
+          </div>
+        ))}
         <div className="orderhistory">place your order now!</div>
       </div>
     </>
