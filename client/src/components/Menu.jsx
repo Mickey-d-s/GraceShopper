b34bad54b99ec45cdb3793a1e1b0b2bfbd21906c
Brandon <bbrungardt02@gmail.com>
2023-07-10 11:22:18 -0500

    disable button when in cart

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -5,10 +5,19 @@ import {
   getUserShoppingCart,
 } from "../api/menu";
 
+const isInCart = (product_id) => {
+  // return true or false if the product with id or productId
+  // is in your local storage cart
+  // Retrieve the cart items from the local storage and check if the product is in the cart
+  const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  return cartItems.some((item) => item.product_id === product_id);
+};
+
 export default function allProducts({ setCartItemCount }) {
   const [products, setProducts] = useState([]);
   const [cart_id, setShoppingCartId] = useState(null);
   const [counts, setCounts] = useState({});
+  const [insideCart, setInsideCart] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -27,6 +36,7 @@ export default function allProducts({ setCartItemCount }) {
 
   const addToCart = async (shoppingcart_id, product_id, count) => {
     // Ensure shoppingcart_id is defined before adding to cart
+    //if product_id is already in cart disable button unless quantity is changed
     if (shoppingcart_id) {
       try {
         const cartItem = await addItemToCart({
@@ -68,48 +78,58 @@ export default function allProducts({ setCartItemCount }) {
                 <p>{product.description}</p>
                 <p>${product.price}</p>
                 <div>
-                  <div>
-                    <button
-                      className="count-button"
-                      onClick={() => {
-                        const currentCount = counts[product.product_id] || 1;
-                        setCounts({
-                          ...counts,
-                          [product.product_id]:
-                            currentCount > 1 ? currentCount - 1 : 1,
-                        });
-                      }}
-                    >
-                      -
-                    </button>
-                    <span className="count">
-                      {counts[product.product_id] || 1}
-                    </span>
-                    <button
-                      className="count-button"
-                      onClick={() => {
-                        const currentCount = counts[product.product_id] || 1;
-                        setCounts({
-                          ...counts,
-                          [product.product_id]: currentCount + 1,
-                        });
-                      }}
-                    >
-                      +
-                    </button>
-                  </div>
-                  <button
-                    id="addToCartButton"
-                    onClick={() =>
-                      addToCart(
-                        cart_id,
-                        product.product_id,
-                        counts[product.product_id] || 1
-                      )
-                    }
-                  >
-                    Add to Cart
-                  </button>
+                  {isInCart(product.product_id) ? (
+                    // Display stuff if product is in cart
+                    <h1>fire</h1>
+                  ) : (
+                    // Don't display stuff if product is not in cart
+                    <div>
+                      <div>
+                        <button
+                          className="count-button"
+                          onClick={() => {
+                            const currentCount =
+                              counts[product.product_id] || 1;
+                            setCounts({
+                              ...counts,
+                              [product.product_id]:
+                                currentCount > 1 ? currentCount - 1 : 1,
+                            });
+                          }}
+                        >
+                          -
+                        </button>
+                        <span className="count">
+                          {counts[product.product_id] || 1}
+                        </span>
+                        <button
+                          className="count-button"
+                          onClick={() => {
+                            const currentCount =
+                              counts[product.product_id] || 1;
+                            setCounts({
+                              ...counts,
+                              [product.product_id]: currentCount + 1,
+                            });
+                          }}
+                        >
+                          +
+                        </button>
+                      </div>
+                      <button
+                        id="addToCartButton"
+                        onClick={() =>
+                          addToCart(
+                            cart_id,
+                            product.product_id,
+                            counts[product.product_id] || 1
+                          )
+                        }
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
