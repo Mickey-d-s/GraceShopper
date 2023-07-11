4ca5f016deeb80c35dbfbce5ca12c81e9082ddce
Brandon <bbrungardt02@gmail.com>
2023-07-11 17:27:06 -0500

    commented out console logs

--- client/src/api/profile.js
+++ client/src/api/profile.js
@@ -2,7 +2,7 @@ export async function fetchAllShoppingCarts(shoppingcart_id) {
   try {
     const response = await fetch(`/api/shoppingcart/:${shoppingcart_id}`);
     const result = await response.json();
-    console.log(result);
+    // console.log(result);
     return result;
   } catch (error) {
     console.error(error);
