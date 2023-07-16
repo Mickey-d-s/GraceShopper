3a752145999fa9be4707e45b27b2282f07924360
Brandon <bbrungardt02@gmail.com>
2023-07-16 14:08:59 -0500

    empty order history message

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -39,26 +39,30 @@ export default function Profile() {
           <hr></hr>
           <div className="orderhistory">
             <h2> Order History</h2>
-            <h3 className="historyProducts">Your Recent Purchased Items</h3>
-
-            {shoppingCarts.map((shoppingCart) => (
-              <div id="scroll">
-                <div
-                  key={shoppingCart.shoppingcart_id}
-                  className="ShoppingCart"
-                >
-                  <h2>Order No. {shoppingCart.shoppingcart_id}</h2>
-                  <h3>Total: ${calculateTotalPrice(shoppingCart)}</h3>
-                  {shoppingCart.products.map((item) => (
-                    <div key={item.item_id}>
-                      <h4>{item.name}</h4>
-                      <p>Qty: {item.qty}</p>
-                      <p>Cost Per Item: ${item.price}</p>
+            {shoppingCarts.length > 0 ? (
+              <div>
+                {shoppingCarts.map((shoppingCart) => (
+                  <div id="scroll">
+                    <div
+                      key={shoppingCart.shoppingcart_id}
+                      className="ShoppingCart"
+                    >
+                      <h2>Order No. {shoppingCart.shoppingcart_id}</h2>
+                      <h3>Total: ${calculateTotalPrice(shoppingCart)}</h3>
+                      {shoppingCart.products.map((item) => (
+                        <div key={item.item_id}>
+                          <h4>{item.name}</h4>
+                          <p>Qty: {item.qty}</p>
+                          <p>Cost Per Item: ${item.price}</p>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
-            ))}
+            ) : (
+              <p>Your Order History is empty 🤕</p>
+            )}
           </div>
         </div>
       </div>
